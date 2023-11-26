1ced943fed25c5b7ded6d9e93cb16ac1d1c51831
Gagan <kbgagan@pbgtechservices.com>
2023-11-26 09:11:50 +0530

    gh

--- src/app/colleges/[pagination]/[category]/layout.js
+++ src/app/colleges/[pagination]/[category]/layout.js
@@ -14,3 +14,29 @@ export default function RootLayout({ children }) {
     </html>
   )
 }
+
+
+{/* <html lang="en" className="body-light">
+      <head>
+        <title>
+          {title && description
+            ? `${title} | ${description}`
+            : 'Study| Get 100% Fee Concession In Reputed PU Science College Based on Entrance Exam or 10th Result'}
+        </title>
+        <link rel="icon" href="./favicon.ico" />
+
+        <meta name="twitter:url" content={canonicalURL} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={thumbnail} />
+
+        <meta property="og:url" content={canonicalURL} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={thumbnail} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={title} />
+        <meta property="og:image:alt" content={description} />
+      </head> */}
